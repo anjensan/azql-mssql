154f71072cac563d77552a76dd1f5e5d90ee4d2d
Andrei Zhlobich <a.zhlobich@intetics.com>
2012-10-16 21:50:00 +1200

    Factory for connections (stolen from korma)

--- src/azql_mssql/core.clj
+++ src/azql_mssql/core.clj
@@ -1,47 +1,22 @@
 (ns azql-mssql.core
-  (:use [azql dialect render emit])
-  (:require [clojure.string :as s]))
+  (:require azql-mssql.dialect))
 
-(register-dialect ::mssql)
+; stolen from 'korma'
+(defn mssql-db
+  "Create a database specification for a mssql database. 
+   Opts should include keys for :db, :user, and :password. 
+   You can also optionally set host and port."
+  [{:keys [user password db host port] :as opts}]
+  (let [host (or (:host opts) "localhost")
+        port (or (:port opts) 5432)
+        user (or user "dbuser")
+        password (or password "dbpassword")
+        db (or (:db opts) "")]
+    (merge 
+      {:classname "net.sourceforge.jtds.jdbc.Driver"
+       :subprotocol "jtds:sqlserver"
+       :user        user
+       :password    password
+       :subname (str "//" host ":" port "/" db)}
+      opts)))
 
-(def BETWEEN (raw "BETWEEN"))
-(def TOP (raw "TOP"))
-
-(defmethod guess-dialect :jtds
-  [_]
-  ::mssql)
-
-(defmethod naming-strategy ::mssql
-  []
-  {:entity (fn [x] (str \[ x \]))
-   :keyword s/lower-case})
-
-(defmethod render-limit ::mssql
-  [_]
-  NONE)
-
-(defn render-select-body
-  [relation render-orderby?]
-  [SELECT
-   (render-modifier relation)
-   (render-fields relation)
-   (render-from relation)
-   (render-where relation)
-   (if render-orderby? (render-order relation) NONE)
-   (render-group relation)
-   (renger-having relation)])
-
-(defmethod render-select ::mssql
-  [{:keys [offset limit] :as relation}]
-  (if (or offset limit)
-    (let [start (or offset 1)
-          end (+ -1 start (or limit (max-limit-value)))]
-      [(raw "SELECT * FROM (SELECT *, row_number() OVER (")
-       (if (:order relation)
-         (render-order relation)
-         (raw "ORDER BY CURRENT_TIMESTAMP"))
-       (raw ") AS _azql_rownum FROM (")
-       (render-select-body relation false)
-       (raw ") AS _azql_query ) AS _azql_numberedqeery")
-       WHERE :_azql_numberedqeery._azql_rownum BETWEEN start AND end])
-    (render-select-body relation true)))
